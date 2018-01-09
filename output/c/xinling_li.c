35aad15e56c5f28812152cd6de55397ce915715e
Michael Wiest <michaelwiest@Michaels-MacBook-Pro-2.local>
2018-01-08 21:28:43 -0800

    marked files for deletion/ignore

--- output/c/xinling_li.c
+++
@@ -1,9 +0,0 @@
-def PatternCount(Text, Pattern):
-  count=0
-  i=0
-  for i in range(0,1054):
-      mer=Text[i:i+9]
-      if mer==Pattern:
-        count=count+1
-      i=i+1
-  return count
--- output/javascript/mustaaf_khan.js
+++
@@ -1,7 +0,0 @@
-def PatternCount(Text, Pattern):
-	count = 0
-	for i in range(0, len(text) - len(pattern) + 1):
-		k_mer = text[i: i + len(pattern)]
-		if(k_mer) == pattern:
-			count+= 1
-	return count
